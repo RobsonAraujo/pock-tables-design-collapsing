4661bf432279205b834a8bb5f08446a2f9576ddd
Robson Araujo carmo <robsonaraujocarmo@Robsons-MacBook-Pro.local>
2022-07-14 15:40:13 -0300

    added animation to collapisible

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
+import "./styles.css"
 
 const Collapsible = ({ children, open }) => {
-  return <>{open && children}</>;
+
+
+  const [height, setHeight] = useState(
+    open ? undefined : 0
+  );
+
+  const ref = useRef();
+
+  useEffect(() => {
+    if (!height || !open || !ref.current) return undefined;
+
+    const resizeObserver = new ResizeObserver((el) => {
+      setHeight(el[0].contentRect.height);
+    });
+    resizeObserver.observe(ref.current);
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [height, open]);
+
+  useEffect(() => {
+    if (open) setHeight(ref.current?.getBoundingClientRect().height);
+    else setHeight(0);
+  }, [open]);
+
+
+ return <div className="collapsible-animation" style={{ height }}>
+  <div ref={ref}>
+    {children}
+  </div>
+</div>
 };
 
 export default Collapsible;
